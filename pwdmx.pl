4ce8f929a8cdd18e043cb592882bf0a5f7a5080f
Pnomohr Waggs <pfwaggs@townm.org>
2016-06-04 16:46:50 -0400

    first functional release.  docs and Readme coming soon.

--- pwdmx.pl
+++ pwdmx.pl
@@ -29,6 +29,8 @@ use Data::Printer;
 
 #ZZZ
 
+my $seed = srand($$);
+
 my %chars = (
     upper => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
     lower => 'abcdefghijklmnopqrstuvwxyz',
@@ -40,24 +42,39 @@ $chars{Alnum} = $chars{upper}.$chars{digit};
 $chars{char} = $chars{upper}.$chars{lower}.$chars{digit}.$chars{special};
 # remake chars as a hash of arrays.
 $chars{$_} = [split //, $chars{$_}] for keys %chars;
-p %chars;
-die;
 
-my $pattern = udls;
-my $count = 100;
-GetOptions( 'pattern|p=s'=>\$pattern, 'count|c=i'=>\$count ) or die 'something goes here';
-my @base = split //, $pattern;
-p @base;
-
-my @list;
-for (split //,$pattern) {
-    when (/c/) {$key = 'char'}
-    when (/a/) {$key = 'alnum'}
-    when (/A/) {$key = 'Alnum'}
-    when (/u/) {$key = 'upper'}
-    when (/l/) {$key = 'lower'}
-    when (/d/) {$key = 'digit'}
-    when (/s/) {$key = 'special'}
-    push @list, $chars{$key}[];
+# get_Block #AAA
+sub get_Block {
+    my $pattern = shift;
+    my @list;
+    for my $key (split //, $pattern) {
+        my $length = 0;
+        given ($key) {
+            when (/c/) {$key = 'char';    $length = @{$chars{$key}}; }
+            when (/a/) {$key = 'alnum';   $length = @{$chars{$key}}; }
+            when (/A/) {$key = 'Alnum';   $length = @{$chars{$key}}; }
+            when (/u/) {$key = 'upper';   $length = @{$chars{$key}}; }
+            when (/l/) {$key = 'lower';   $length = @{$chars{$key}}; }
+            when (/d/) {$key = 'digit';   $length = @{$chars{$key}}; }
+            when (/s/) {$key = 'special'; $length = @{$chars{$key}}; }
+        }
+        push @list, $chars{$key}[int rand($length)];
+    }
+    return join('',@list);
 }
-p @list;
+#ZZZ
+
+my $pattern = 'udls';
+my $rows = 10;
+my $cols = 10;
+my $space = 3;
+GetOptions( 'pattern|p=s'=>\$pattern, 'rows|r=i'=>\$rows, 'cols|c=i'=>\$cols, 'space|s=i'=>\$space ) or die 'something goes here';
+$space = ' 'x$space;
+printf "%2s %s%s\n", '\\', $space, join($space,map {sprintf "%*d",length $pattern,$_} (1..$rows));
+for my $row (1..$rows) {
+    my @line = ();
+    for my $col (1..$cols) {
+        push @line, get_Block($pattern);
+    }
+    printf "%2d:%s%s\n", $row, $space, join($space,@line);
+}
